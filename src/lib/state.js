e2240baad65a0a1ce99269efa2fbcbec27a6b41c
Justin Makeig <jmpublic@makeig.com>
2023-01-11 03:51:00 +0000

    Adds async store

---
+++ src/lib/state.js
@@ -0,0 +1,56 @@
+/**
+ * @template W
+ * @typedef {import("svelte/store").Writable<W>} Writable<W>
+ */
+
+/**
+ * @template R
+ * @typedef {import("svelte/store").Readable<R>} Readable<R>
+ */
+
+/**
+ * @template A
+ * @typedef { Writable <A> | Readable<A> } AnyStore<A>
+ */
+
+/**
+ * 
+ */
+
+/**
+ * @template In
+ * @template Out
+ * @param {AnyStore<In>} store
+ * @param {(value: In) => Promise<Out>} transform
+ */
+export function derived_async(store, transform) {
+	/** @type {Array<(value: Out) => void>} */
+	let subscribers = [];
+
+	store.subscribe((value) =>
+		transform(value)
+			.then((result) => {
+				console.log('sub result', result);
+				for (const f of subscribers) {
+					f(result);
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			})
+	); // TODO: .catch()
+	return {
+		/**
+		 * @param {(value: Out) => void} f
+		 * @returns {() => void }
+		 */
+		subscribe(f) {
+			console.log('subscribed');
+			subscribers.push(f);
+			return () => {
+				// Is the identity of f sufficient here?
+				subscribers = subscribers.filter((item) => item !== f);
+			};
+		}
+	};
+}
